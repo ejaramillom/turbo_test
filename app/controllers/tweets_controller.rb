27a91c3a1bda3790a10703184ca99c3f4c38011f
ejaramillom <ejaramilo@hotmail.com>
2024-08-21 11:28:51 -0500

    feat(turbo_test): Add turbo frames to edit and show

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,40 +1,35 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: %i[ show edit update destroy ]
 
-  # GET /tweets or /tweets.json
   def index
     @tweets = Tweet.all
   end
 
-  # GET /tweets/1 or /tweets/1.json
   def show
   end
 
-  # GET /tweets/new
   def new
     @tweet = Tweet.new
   end
 
-  # GET /tweets/1/edit
   def edit
   end
 
-  # POST /tweets or /tweets.json
   def create
     @tweet = Tweet.new(tweet_params)
 
     respond_to do |format|
       if @tweet.save
-        format.html { redirect_to tweet_url(@tweet), notice: "Tweet was successfully created." }
+        format.html { redirect_to tweet_path(@tweet), notice: "Tweet was successfully created." }
         format.json { render :show, status: :created, location: @tweet }
       else
+        format.turbo_stream { render turbo_stream: turbo_stream.replace(@tweet, partial: "tweets/form", locals: { tweet: @tweet }) }
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @tweet.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /tweets/1 or /tweets/1.json
   def update
     respond_to do |format|
       if @tweet.update(tweet_params)
@@ -47,7 +42,6 @@ class TweetsController < ApplicationController
     end
   end
 
-  # DELETE /tweets/1 or /tweets/1.json
   def destroy
     @tweet.destroy!
 
@@ -58,13 +52,12 @@ class TweetsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_tweet
       @tweet = Tweet.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def tweet_params
-      params.fetch(:tweet, {})
+      params.require(:tweet).permit(:body)
     end
 end
